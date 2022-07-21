f28164ed1f4af183fb2f8ced1c7e408897bcb42e
Denim Hodgson <denimhodgson@gmail.com>
2022-07-21 15:12:25 +0800

    Some additions
    NavItem now changes color when hovered to match the theme, fixed CSS issue with images showing zoomed in

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -2,7 +2,13 @@ import { Link } from 'react-scroll';
 
 const NavItem = (props) => {
   return (
-    <li className={props.isStyle ? 'py-6 text-4xl' : ''}>
+    <li
+      className={
+        props.isStyle
+          ? 'py-6 text-4xl hover:text-red-400'
+          : 'hover:text-red-400'
+      }
+    >
       <Link
         onClick={props.handleClick}
         to={props.name}
