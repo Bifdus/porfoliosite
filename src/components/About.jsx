1ae20dc261a98b3fc3e832d23321473b485eeca6
Denim Hodgson <denimhodgson@gmail.com>
2022-07-25 16:58:55 +0800

    more text updates, fixes for phones

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
     >
       <div className='flex flex-col justify-center items-center w-full'>
         <img id='profile' src={ProfileImg} alt='Denim Hodgson Profile' />
-        <div className='max-w-[1000px] w-full h-full grid grid-cols-2 gap-8'>
+        <div className='max-w-[1000px] w-full h-full grid grid-cols-2 gap-8 about-section'>
           <div className='sm:text-right pb-8 pl-4'>
             <p className='text-4xl font-bold inline border-b-4 border-fuchsia-800'>
               About
@@ -20,18 +20,19 @@ const About = () => {
             </h2>
           </div>
           <div>
-            <p>
-              During my time working as IT and manufacturing hydraulic hoses I
-              stumbled upon a strong passion for web development. This has
-              driven me to change my career as in particular, I enjoy working
-              with industry standard frameworks to create clean and functional
-              applications that are deployed to cloud services. In pursuing a
-              career as a web developer, I am seeking opportunities which will
-              help me to improve my skills in front and back end development and
-              enable me to explore new and emerging techniques / technologies.
-              In addition to my technical capabilities, I prioritise customer
-              experience and am passionate about relationship building. Outside
-              of work, I enjoy boxing to keep fit
+            <p className='mx-4'>
+              During my time working as an IT support technician and
+              manufacturing hydraulic hoses, I stumbled upon a strong passion
+              for web development. This has driven me to change my career as in
+              particular, I enjoy working with industry standard frameworks to
+              create clean and functional applications that are deployed to
+              cloud services. In pursuing a career as a web developer, I am
+              seeking opportunities which will help me to improve my skills in
+              front and back end development and enable me to explore new and
+              emerging techniques / technologies. In addition to my technical
+              capabilities, I prioritise customer experience and am passionate
+              about relationship building. Outside of work, I enjoy boxing to
+              keep fit
             </p>
           </div>
         </div>
