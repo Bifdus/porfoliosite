4dab56ad00cde59aeb505e64f1cf47ee0b14ea4d
Denim Hodgson <denimhodgson@gmail.com>
2022-08-02 19:27:04 +0800

    Better formatting
    Shows the webapplication project, then shows how it works in the actual game after db has been updated

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -8,17 +8,17 @@ export const projectData = [
   },
   {
     id: 2,
-    title: 'Static Targets Boxing',
-    text: 'Static target game mode where patients can play a clinician created target practice program. Left and Right hand restrictions able to be set as well.',
-    embedId: '4b6f9255-544d-45e7-9dc7-fcad99a63b53',
-    link: 'https://www.veed.io/view/4b6f9255-544d-45e7-9dc7-fcad99a63b53',
-  },
-  {
-    id: 3,
-    title: 'Web Interface for Clinician Setup',
+    title: 'Webapp - Boxing Static Target Setup',
     text: 'I added the boxing target functionality to the existing web application. The server is hosted on a LAMP stack.',
     embedId: '84beca59-6d02-432c-b865-cbcc18d8bf9c',
     link: 'https://www.veed.io/view/84beca59-6d02-432c-b865-cbcc18d8bf9c',
+  },
+  {
+    id: 3,
+    title: 'Static Targets Boxing',
+    text: 'Static target game mode where patients can play a static practice program that is specified per patient through the clinician web interface.',
+    embedId: '4b6f9255-544d-45e7-9dc7-fcad99a63b53',
+    link: 'https://www.veed.io/view/4b6f9255-544d-45e7-9dc7-fcad99a63b53',
   },
   {
     id: 4,
