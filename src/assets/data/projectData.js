6d5cb653b14489901de4058afc31015e56a20a19
Denim Hodgson <denimhodgson@gmail.com>
2023-01-21 14:25:22 +0800

    New project added, shifted the order in which they are shown

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -1,32 +1,41 @@
 export const projectData = [
   {
     id: 1,
-    title: 'Multiplayer Boxing Game',
-    text: 'The multiplayer component of the game was set up using Photon and Photon Voice. The game mode allows players to join lobbies and interact with each other. Includes RPCs for collision detection.',
-    embedId: '8bbf41c9-b9b7-4652-8699-005a5183c6dd',
-    link: 'https://www.veed.io/view/8bbf41c9-b9b7-4652-8699-005a5183c6dd',
-  },
-  {
-    id: 2,
-    title: 'Webapp - Boxing Static Target Setup',
-    text: 'I added the boxing target functionality to the existing web application. The server is hosted on a LAMP stack.',
-    embedId: '84beca59-6d02-432c-b865-cbcc18d8bf9c',
-    link: 'https://www.veed.io/view/84beca59-6d02-432c-b865-cbcc18d8bf9c',
-  },
-  {
-    id: 3,
-    title: 'Static Targets Boxing',
-    text: 'Static target game mode where patients can play a static practice program that is unique per patient, set up via the clinician web interface.',
-    embedId: '4b6f9255-544d-45e7-9dc7-fcad99a63b53',
-    link: 'https://www.veed.io/view/4b6f9255-544d-45e7-9dc7-fcad99a63b53',
-  },
-  {
-    id: 4,
     title: 'Portfolio Website',
     text: 'This website has been created using React JS and is built and deployed from my Github repo (link below) using Netlify. Each section contains components that are independently styled. Particles generated via tsparticles.',
     image:
       'https://res.cloudinary.com/dlyygpzwl/image/upload/v1658726223/Website/website_akf2x5.jpg',
     link: 'https://github.com/Bifdus/portfolio-site',
     buttontext: 'Github',
+  },
+  {
+    id: 2,
+    title: 'Static Website (IN PROGRESS)',
+    text: 'This static website project that I am collaborating on with a client is utilizing NextJS, React and Typescript as its main technologies. The styling is being done using Material-UI v5, providing a sleek and modern look.',
+    image:
+      'https://res.cloudinary.com/dlyygpzwl/image/upload/v1674282092/Website/djelectrics_achuz0.png',
+    link: 'https://snazzy-stardust-17873c.netlify.app/',
+    buttontext: 'View',
+  },
+  {
+    id: 3,
+    title: 'Multiplayer Boxing Game',
+    text: 'The multiplayer component of the game was set up using Photon and Photon Voice. The game mode allows players to join lobbies and interact with each other. Includes RPCs for collision detection.',
+    embedId: '8bbf41c9-b9b7-4652-8699-005a5183c6dd',
+    link: 'https://www.veed.io/view/8bbf41c9-b9b7-4652-8699-005a5183c6dd',
+  },
+  {
+    id: 4,
+    title: 'Webapp - Boxing Static Target Setup',
+    text: 'I added the boxing target functionality to the existing web application. The server is hosted on a LAMP stack.',
+    embedId: '84beca59-6d02-432c-b865-cbcc18d8bf9c',
+    link: 'https://www.veed.io/view/84beca59-6d02-432c-b865-cbcc18d8bf9c',
+  },
+  {
+    id: 5,
+    title: 'Static Targets Boxing',
+    text: 'Static target game mode where patients can play a static practice program that is unique per patient, set up via the clinician web interface.',
+    embedId: '4b6f9255-544d-45e7-9dc7-fcad99a63b53',
+    link: 'https://www.veed.io/view/4b6f9255-544d-45e7-9dc7-fcad99a63b53',
   },
 ];
